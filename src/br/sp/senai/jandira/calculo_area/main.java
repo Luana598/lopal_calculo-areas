1a1d6cc1b29d4c8927ff20b1227edbe406afd9dd
25132776 <25132776@DSN1271262514.cfp127aluno.local>
2025-03-17 11:25:55 -0300

    Adicionado o menu

--- src/br/sp/senai/jandira/calculo_area/main.java
+++ src/br/sp/senai/jandira/calculo_area/main.java
@@ -1,39 +1,14 @@
 package br.sp.senai.jandira.calculo_area;
 
+import java.util.Scanner;
+
 import br.sp.senai.jandira.calculo_area.model.*;
 
 public class main {
 	
 	public static void main(String[] args) {
-		Retangulo formaR  = new Retangulo();
 		
-		formaR.setAltura(134);
-		formaR.setBase(120);
-		formaR.mostrarDados();
-		
-		Circunferencia formaC = new Circunferencia();
-		
-		formaC.setRaio(4);
-		formaC.mostrarDados();
-		
-		Quadrado formaQ = new Quadrado();
-		
-		formaQ.setLado(10);
-		formaQ.mostrarDados();
-		
-		Triangulo formaT = new Triangulo();
-		
-		formaT.setAltura(5);
-		formaT.setBase(10);
-		formaT.mostrarDados();
-		
-		Trapezio formaTr = new Trapezio();
-		
-		formaTr.setBaseMaior(5);
-		formaTr.setBaseMenor(10);
-		formaTr.setAltura(8);
-		formaTr.mostrarDados();
+		Menu.criarMenu();	
 	}
-
 	
 }
